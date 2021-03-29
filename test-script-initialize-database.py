c7bfb1ad990a20b0c0a797e0ad6892575db36f1a
Andrew <andrewbduong@gmail.com>
2021-03-28 19:47:37 -0700

    Added comments

--- test-script-initialize-database.py
+++ test-script-initialize-database.py
@@ -1,3 +1,5 @@
+#This is a script to create the database that we will be using for our other test scripts
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,20 +10,25 @@ class CreateDatabase(unittest.TestCase):
         self.driver = webdriver.Firefox()
 
     def test_initialize_database(self):
+        #Logging into the admin portal of ParaBank to initialize all the back end stuff
         driver = self.driver
         url="https://parabank.parasoft.com/parabank/admin.htm"
         driver.get(url)
         self.assertIn("ParaBank", driver.title)
 
+        #Making sure that Data Access Mode is set to REST (XML)
         elem = driver.find_element_by_xpath("//input[@id='accessMode2'][@class='input'][@name='accessMode'][@type='radio'][@value='restxml']")
         elem.click()
 
+        #Clicking Submit to save changes
         elem = driver.find_element_by_xpath("//input[@class='button'][@type='submit'][@value='Submit']")
         elem.click()
 
+        #Cleaning database first in case there was already data inside
         elem = driver.find_element_by_xpath("//button[@class='button'][@type='submit'][@name='action'][@value='CLEAN']")
         elem.click()
 
+        #Initializing database for use with our other test scripts
         elem = driver.find_element_by_xpath("//button[@class='button'][@type='submit'][@name='action'][@value='INIT']")
         elem.click()
 
